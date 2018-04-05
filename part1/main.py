4739b1a1b84f4061265f42f095d1862a402d5520
Caleb Lewis <caleb_dre@yahoo.com>
2018-04-05 19:42:54 -0400

    dfd

--- part1/main.py
+++ part1/main.py
@@ -1,3 +1,10 @@
+import matplotlib
+matplotlib.use('Qt5Agg')
+# This should be done before `import matplotlib.pyplot`
+# 'Qt4Agg' for PyQt4 or PySide, 'Qt5Agg' for PyQt5
+import matplotlib.pyplot as plt
+import numpy as np
 
-if __name__ == "__main__":
-	print("hello, world")
+t = np.linspace(0, 20, 500)
+plt.plot(t, np.sin(t))
+plt.show()
